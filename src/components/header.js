60bfe04c320b99d7a295960dba4b08f38cca1ce8
Kardbord <tanner.kvarfordt@gmail.com>
2018-07-09 08:31:04 -0600

    Madlib project

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__skew" />
+      <div className="header__border" />
+      <h1>Bottega Madlibs</h1>
+      <p>
+        Fill out the fields below and click the generate button to see the Mad
+        Lib story
+      </p>
+    </div>
+  );
+};
+
+export default Header;
